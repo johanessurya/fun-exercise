1da4a76c2c91ef9f517a4f64489405568fee695a
Johanes Surya <johanes.surya43@gmail.com>
2022-02-14 10:49:28 +0700

    Create google calculator

--- google-adsense-calc/index.php
+++ google-adsense-calc/index.php
@@ -1,3 +1,15 @@
+<?php
+
+/*
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+
+require 'env.php';
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,14 +18,65 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>Checkout example · Bootstrap v5.1</title>
+    <title>Kalkulator Google Adsense - Wevelope</title>
 
     <!-- Bootstrap core CSS -->
-<link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
+<script>
+
+window.onload = function() {
+
+// DOM Ready here
+Vue.createApp({
+  data: function() {
+    return {
+      pv: 1000,
+      ctr: 0.5,
+      cpc: 3000,
+      revDay: 0,
+      revMonth: 0,
+      revYear: 0,
+      clickDay: 0,
+      clickMonth: 0,
+      clickYear: 0
+    }
+  },
+  created: function() {
+    // console.log('created');
+    this.submit();
+  },
+  methods: {
+    submit: function() {
+      click = this.pv * (this.ctr / 100);
+
+      this.revDay = click * this.cpc;
+      this.revMonth = this.revDay * 30;
+      this.revYear = this.revDay * 365;
+
+      this.clickDay = click;
+      this.clickMonth = click * 30;
+      this.clickYear = click * 365;
+    },
+    format: function(number, symbol) {
+      if(symbol == undefined)
+        symbol = '';
+
+      format = '0,0';
+      strNum = numeral(number).format(format);
+
+      // console.log(strNum, symbol, symbol + 'Rp0,0');
+
+      return symbol + strNum;
+    }
+  }
+}).mount('#app')
+
+};
+</script>
 
 <meta name="theme-color" content="#7952b3">
-
-
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -32,8 +95,7 @@
 
   </head>
   <body class="bg-light">
-    
-<div class="container">
+<div class="container" id="app">
   <main>
     <div class="py-5 text-center">
       <h2>Kalkulator Google Adsense</h2>
@@ -41,172 +103,82 @@
 
     <div class="row g-5">
       <div class="col-md-12 col-lg-12">
-        <h4 class="mb-3">Billing address</h4>
-        <form class="needs-validation" novalidate>
+        <form class="needs-validation" novalidate v-on:submit.prevent="submit">
           <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="firstName" class="form-label">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid first name is required.
-              </div>
+            <div class="col-sm-12">
+              <label for="firstName" class="form-label">Pageview Per Hari</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" v-model="pv" required>
             </div>
 
-            <div class="col-sm-6">
-              <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid last name is required.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="username" class="form-label">Username</label>
+            <div class="col-sm-12">
+              <label for="lastName" class="form-label">Click-Through Rate(CTR)</label>
               <div class="input-group has-validation">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="username" placeholder="Username" required>
-              <div class="invalid-feedback">
-                  Your username is required.
-                </div>
+                <input type="text" class="form-control" id="lastName" placeholder="" v-model="ctr" required>
+                <span class="input-group-text">%</span>
               </div>
+
             </div>
 
-            <div class="col-12">
-              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com">
-              <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="address" class="form-label">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
-              <div class="invalid-feedback">
-                Please enter your shipping address.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
-              <select class="form-select" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
-              <div class="invalid-feedback">
-                Please select a valid country.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
-              <select class="form-select" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required>
-              <div class="invalid-feedback">
-                Zip code required.
+            <div class="col-sm-12">
+              <label for="username" class="form-label">Cost Per Click(CPC)</label>
+              <div class="input-group has-validation">
+                <span class="input-group-text">Rp</span>
+                <input type="text" class="form-control" id="username" placeholder="" v-model="cpc" required>
+                <span class="input-group-text">/klik</span>
               </div>
             </div>
           </div>
 
           <hr class="my-4">
 
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="same-address">
-            <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-          </div>
-
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="save-info">
-            <label class="form-check-label" for="save-info">Save this information for next time</label>
-          </div>
-
-          <hr class="my-4">
-
-          <h4 class="mb-3">Payment</h4>
+          <h4 class="mb-3">Rangkuman Perhitungan</h4>
 
           <div class="my-3">
-            <div class="form-check">
-              <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
-              <label class="form-check-label" for="credit">Credit card</label>
-            </div>
-            <div class="form-check">
-              <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="debit">Debit card</label>
-            </div>
-            <div class="form-check">
-              <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="paypal">PayPal</label>
-            </div>
-          </div>
 
-          <div class="row gy-3">
-            <div class="col-md-6">
-              <label for="cc-name" class="form-label">Name on card</label>
-              <input type="text" class="form-control" id="cc-name" placeholder="" required>
-              <small class="text-muted">Full name as displayed on card</small>
-              <div class="invalid-feedback">
-                Name on card is required
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <label for="cc-number" class="form-label">Credit card number</label>
-              <input type="text" class="form-control" id="cc-number" placeholder="" required>
-              <div class="invalid-feedback">
-                Credit card number is required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-expiration" class="form-label">Expiration</label>
-              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-              <div class="invalid-feedback">
-                Expiration date required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-cvv" class="form-label">CVV</label>
-              <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-              <div class="invalid-feedback">
-                Security code required
-              </div>
-            </div>
-          </div>
+<table class="table">
+  <tbody>
+    <tr>
+      <td>Penghasilan Harian</td>
+      <td class="text-end">{{ format(revDay, 'Rp') }}</td>
+    </tr>
+    <tr>
+      <td>Penghasilan Bulanan</td>
+      <td class="text-end">{{ format(revMonth, 'Rp') }}</td>
+    </tr>
+    <tr>
+      <td>Penghasilan Tahunan</td>
+      <td class="text-end">{{ format(revYear, 'Rp') }}</td>
+    </tr>
+    <tr>
+      <td>Jumlah Klik Per Hari</td>
+      <td class="text-end">{{ format(clickDay) }} klik</td>
+    </tr>
+    <tr>
+      <td>Jumlah Klik Per Bulan</td>
+      <td class="text-end">{{ format(clickMonth) }} klik</td>
+    </tr>
+    <tr>
+      <td>Jumlah Klik Per Tahun</td>
+      <td class="text-end">{{ format(clickYear) }} klik</td>
+    </tr>
+  </tbody>
+</table>
 
           <hr class="my-4">
 
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Hitung</button>
         </form>
       </div>
     </div>
   </main>
 
-  <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; 2017–2021 Company Name</p>
+  <footer class="text-muted text-center text-small">
+    <p class="mb-1">&copy; 2016 - 2022 <a href="<?php echo FOOTER_URL; ?>">Wevelope</a> - <?php echo FOOTER_TAGLINE; ?></p>
     <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Privacy</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Support</a></li>
+      <li class="list-inline-item"><a href="<?php echo ARTICLE_URL ?>">Penjelasan Cara Menghitung Google Adsense</a></li>
     </ul>
   </footer>
 </div>
-
 
     <script src="../bower_components/bootstrap/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
